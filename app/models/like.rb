a8da66bc80bcf7baf0b37c7546951bdd3dfc7f81
borvux <bennyjoram@gmail.com>
2025-02-25 13:52:44 -0600

    created associations for like

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@
 #
 # Table name: likes
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  fan_id     :integer
@@ -21,6 +21,8 @@ class Like < ApplicationRecord
   ## Direct associations
 
   # Like#fan: returns a row from the users table associated to this like by the fan_id column
+  belongs_to(:fan, class_name: "User", foreign_key: "fan_id")
 
   # Like#photo: returns a row from the photo table associated to this like by the photo_id column
+  belongs_to(:photo, class_name: "Photo", foreign_key: "photo_id")
 end
