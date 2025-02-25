ac7db8af6bbe2941daab138be33c38b6bb435779
borvux <bennyjoram@gmail.com>
2025-02-25 13:52:02 -0600

    created relations in comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@
 #
 # Table name: comments
 #
-#  id         :integer          not null, primary key
+#  id         :bigint           not null, primary key
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -18,6 +18,8 @@ class Comment < ApplicationRecord
   ## Direct associations
 
   # Comment#commenter: returns a row from the users table associated to this comment by the author_id column
+  belongs_to(:commenter, class_name: "User", foreign_key: "author_id")
 
   # Comment#photo: returns a row from the photos table associated to this comment by the photo_id column
+  belongs_to(:photo, class_name: "Photo", foreign_key: "photo_id")
 end
