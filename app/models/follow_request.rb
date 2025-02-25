084aebf9459a0290716c16e17470597fef258605
borvux <bennyjoram@gmail.com>
2025-02-25 13:52:31 -0600

    created associations for follow_request

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -2,7 +2,7 @@
 #
 # Table name: follow_requests
 #
-#  id           :integer          not null, primary key
+#  id           :bigint           not null, primary key
 #  status       :string
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
@@ -22,6 +22,8 @@ class FollowRequest < ApplicationRecord
   ## Direct associations
 
   # FollowRequest#sender: returns a row from the users table associated to this follow request by the sender_id column
+  belongs_to(:sender, class_name: "User", foreign_key: "sender_id")
 
   # FollowRequest#recipient: returns a row from the users table associated to this follow request by the recipient_id column
+  belongs_to(:recipient, class_name: "User", foreign_key: "recipient_id")
 end
